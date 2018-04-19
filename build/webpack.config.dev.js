6b137eed6ac6885a5a52851917dffb489f0de3ff
annecylee <annecylee@gmail.com>
2018-04-18 23:32:40 -0700

    Static Assets, testing and style

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,6 +1,16 @@
 'use strict'
+
+const path = require('path')
+
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ExtractPlugin = require('extract-text-webpack-plugin')
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
 module.exports = {
   mode: 'development',
   devServer: {
@@ -16,8 +26,8 @@ module.exports = {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+            'scss': ExtractPlugin.extract('vue-style-loader!css-loader!sass-loader'),
+            'sass': ExtractPlugin.extract('vue-style-loader!css-loader!sass-loader?indentedSyntax')
           }
         }
       },
@@ -42,6 +52,12 @@ module.exports = {
       filename: 'index.html',
       template: 'index.html',
       inject: true
-    })
+    }),
+    new CopyWebpackPlugin([{
+      from: resolve('static/img'),
+      to: resolve('dist/static/img'),
+      toType: 'dir'
+    }]),
+    new ExtractPlugin('main.css')
   ]
 }
